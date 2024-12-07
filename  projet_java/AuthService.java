62354960b4c4da70a97715cc1d5eaeac34456dd2
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 21:54:01 +0100

    Méthode pour authentifier un utilisateur et récupérer son rôle

---  projet_java/AuthService.java
+++  projet_java/AuthService.java
@@ -3,5 +3,25 @@ import java.sql.*;
 
 public class AuthService {
 
-    
+    // Méthode pour authentifier un utilisateur et récupérer son rôle
+	public User authenticate(String username, String password) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+                
+                ResultSet resultSet = preparedStatement.executeQuery();
+                
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("id");  
+                    String role = resultSet.getString("role");
+                    return new User(id, username, password, role);  
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; 
+    }
 }
