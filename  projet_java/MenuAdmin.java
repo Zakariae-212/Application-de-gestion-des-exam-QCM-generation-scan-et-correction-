3d13a91d858c542130b163e685bf99ce03bbc790
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 21:43:00 +0100

    Ajouter le menu administrateur pour la gestion des utilisateurs

---
+++  projet_java/MenuAdmin.java
@@ -0,0 +1,13 @@
+
+ package projet_java;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class MenuAdmin {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    
+    
+}
