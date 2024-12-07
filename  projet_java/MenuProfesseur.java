57c15c5be10f9887a7cb4c6a32f71408ca982243
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 21:17:06 +0100

    Ajouter le menu professeur pour la gestion des examens

---
+++  projet_java/MenuProfesseur.java
@@ -0,0 +1,18 @@
+package projet_java;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class MenuProfesseur {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    
+
+    
+
+    
+
+    
+}
