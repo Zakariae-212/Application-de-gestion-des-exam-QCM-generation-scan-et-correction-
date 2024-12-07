c5ea1a54b5acf00b08763b4ffc25b8d88e479b57
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 21:28:39 +0100

    Initialiser la classe MenuEtudiant pour la gestion des étudiants

---
+++  projet_java/MenuEtudiant .java
@@ -0,0 +1,16 @@
+
+package projet_java;
+
+import java.io.*;
+import java.nio.file.*;
+import java.sql.*;
+import java.util.*;
+
+public class MenuEtudiant {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    
+
+    
+}
