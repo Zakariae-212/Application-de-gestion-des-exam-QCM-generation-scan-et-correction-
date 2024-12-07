7a77a0b5b08d57b1f5d174acbc9cb849530d2b79
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 20:43:15 +0100

    -Création de la classe `DatabaseConnection` pour centraliser la gestion des connexions à la base de données.

---
+++  projet_java/DatabaseConnection.java
@@ -0,0 +1,23 @@
+
+package projet_java;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/qcm";
+    private static final String DB_USER = "root"; 
+    private static final String DB_PASSWORD = ""; 
+
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Erreur lors de la connexion à la base de données.");
+        }
+    }
+}
