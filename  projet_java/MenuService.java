57710fde84e346184bbbce68cecfbe340aeb9da9
Zakariae-212 <roomvortex5@gmail.com>
2024-12-07 21:51:08 +0100

    Méthode pour afficher le menu en fonction du rôle de l'utilisateur

---  projet_java/MenuService.java
+++  projet_java/MenuService.java
@@ -4,4 +4,23 @@
 public class MenuService {
 
     
+    // Méthode pour afficher le menu en fonction du rôle de l'utilisateur
+    public void displayMenu(User user) {
+        switch (user.getRole()) {
+            case "admin":
+                MenuAdmin menuAdmin = new MenuAdmin();
+                menuAdmin.showMenu();
+                break;
+            case "professeur":
+                MenuProfesseur menuProfesseur = new MenuProfesseur();
+                menuProfesseur.showMenu();
+                break;
+            case "etudiant":
+                MenuEtudiant menuEtudiant = new MenuEtudiant();
+                menuEtudiant.showMenu(user);  
+                break;
+            default:
+                System.out.println("Rôle inconnu.");
+        }
+    }
 }
